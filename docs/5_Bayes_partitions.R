e80db120b2534602c047effb97933f843c51923f
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2021-11-10 09:49:27 -0600

    Update 5_Bayes_partitions.R

--- docs/5_Bayes_partitions.R
+++ docs/5_Bayes_partitions.R
@@ -181,13 +181,14 @@ names(best_2) = c("mobility", "score", "delay", "date_origin", "date_delay", "pa
 
 BEST_PREDICTORS = rbind(best_1, best_2)
 
-score_table_1
-score_table_2
-partition_1
-partition_2
-delay_1
-delay_2
-BEST_PREDICTORS
+report= list(score_table_1
+             score_table_2
+             partition_1
+             partition_2
+             delay_1
+             delay_2
+             BEST_PREDICTORS)
 
+report
 
 #print(list(score_table_1 = score_table_1, score_table_2 = score_table_2, best_predictors = BEST_PREDICTORS))
