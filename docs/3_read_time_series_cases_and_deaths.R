dc8167c58d22513c0c66dac64da5d6cb5b3b67cf
Gabriel Ernesto García Peña <5376741+gegp01@users.noreply.github.com>
2021-11-30 14:32:49 -0600

    Update 3_read_time_series_cases_and_deaths.R

--- docs/3_read_time_series_cases_and_deaths.R
+++ docs/3_read_time_series_cases_and_deaths.R
@@ -48,12 +48,22 @@ N_t1 = c(NY[["cases_ny"]], NA)
 Y_ny = N_t1-N_t0 # diferencia
 covid_ny = na.exclude(Y_ny)
 
+# Calcular la variable de respuesta de decesos en NY (¿Aumento o decremento con respecto a ayer?)
+
+N_t0 = c(NA, NY[["deaths_ny"]])
+N_t1 = c(NY[["deaths_ny"]], NA)
+# Y_ny = N_t1/(N_t0 + 1) # como no puede haber ceros en el    numerador, se suma 1.
+Y_ny = N_t1-N_t0 # diferencia
+deaths_change_ny = na.exclude(Y_ny)
+
 # Actualizar la lista de series de tiempo de New York
 NY = list(cases_ny = C[["cases_ny"]]
           , work_ny_nona = G[["work_ny_nona"]]
           , recreo_ny_nona = G[["recreo_ny_nona"]]
           , residence_ny_nona = G[["residence_ny_nona"]]
-          , covid_ny = covid_ny)
+          , covid_ny = covid_ny
+          , deaths_ny = C[["deaths_ny"]]
+          , death_change_ny = death_change_ny)
 
 # Calcular la variable de respuesta en CDMX (¿Aumentó con respecto a ayer?)
 N_t0 = c(NA, CDMX[["cases_cdmx"]]) # Ciudad de México
@@ -62,12 +72,21 @@ N_t1 = c(CDMX[["cases_cdmx"]], NA)
 Y_cdmx = N_t1-N_t0 # Diferencia N_t1-N_t0
 covid_cdmx = na.exclude(Y_cdmx)
 
+# Calcular la variable de respuesta en decesos CDMX (¿Aumentó con respecto a ayer?)
+N_t0 = c(NA, CDMX[["deaths_cdmx"]]) # Ciudad de México
+N_t1 = c(CDMX[["deaths_cdmx"]], NA)
+# Y_cdmx = N_t1/(N_t0 + 1) # como no puede haber ceros en el    numerador, se suma 1.
+Y_cdmx = N_t1-N_t0 # Diferencia N_t1-N_t0
+deaths_change_cdmx = na.exclude(Y_cdmx)
+
 # Actualizar la lista de series de tiempo de Ciudad de México
 CDMX =  list(cases_cdmx = C[["cases_cdmx"]]
              , work_cdmx_nona = G[["work_cdmx_nona"]]
              , recreo_cdmx_nona = G[["recreo_cdmx_nona"]]
              , residence_cdmx_nona = G[["residence_cdmx_nona"]]
-             , covid_cdmx = covid_cdmx)
+             , covid_cdmx = covid_cdmx
+             , deaths_cdmx = C[["deaths_cdmx"]]
+             , death_change_cdmx = death_change_cdmx)
 
 # Calcular la variable de respuesta en LON (¿Aumentó con respecto a ayer?)
 N_t0 = c(NA, LON[["cases_london"]]) # London
@@ -76,12 +95,22 @@ N_t1 = c(LON[["cases_london"]], NA)
 Y_london = N_t1-N_t0 # Diferencia N_t1-N_t0
 covid_london = na.exclude(Y_london)
 
+# Calcular la variable de respuesta en los decesos en LON (¿Aumentó con respecto a ayer?)
+N_t0 = c(NA, LON[["deaths_london"]]) # London
+N_t1 = c(LON[["deaths_london"]], NA)
+# Y_cdmx = N_t1/(N_t0 + 1) # como no puede haber ceros en el    numerador, se suma 1.
+Y_london = N_t1-N_t0 # Diferencia N_t1-N_t0
+deaths_change_london = na.exclude(Y_london)
+
 # Actualizar la lista de series de tiempo de Londres
 LON =  list(cases_london = C[["cases_london"]]
              , work_london_nona = G[["work_london_nona"]]
              , recreo_london_nona = G[["recreo_london_nona"]]
              , residence_london_nona = G[["residence_london_nona"]]
-             , covid_london = covid_london)
+             , covid_london = covid_london
+             , deaths_london = C[["deaths_london"]]
+             , death_change_london = death_change_london)
+)
 
 # Calcular la variable de respuesta en SAO (¿Aumentó con respecto a ayer?)
 N_t0 = c(NA, SAO[["cases_saopaulo"]]) # Sao Paulo
@@ -90,12 +119,21 @@ N_t1 = c(SAO[["cases_saopaulo"]], NA)
 Y_saopaulo = N_t1-N_t0 # Diferencia N_t1-N_t0
 covid_saopaulo = na.exclude(Y_saopaulo)
 
+# Calcular la variable de respuesta en SAO (¿Aumentó con respecto a ayer?)
+N_t0 = c(NA, SAO[["deaths_saopaulo"]]) # Sao Paulo
+N_t1 = c(SAO[["deaths_saopaulo"]], NA)
+# Y_cdmx = N_t1/(N_t0 + 1) # como no puede haber ceros en el    numerador, se suma 1.
+Y_saopaulo = N_t1-N_t0 # Diferencia N_t1-N_t0
+deaths_change_saopaulo = na.exclude(Y_saopaulo)
+
 # Actualizar la lista de series de tiempo de Sao Paulo
 SAO =  list(cases_saopaulo = C[["cases_saopaulo"]]
             , work_saopaulo_nona = G[["work_saopaulo_nona"]]
             , recreo_saopaulo_nona = G[["recreo_saopaulo_nona"]]
             , residence_saopaulo_nona = G[["residence_saopaulo_nona"]]
-            , covid_saopaulo = covid_saopaulo)
+            , covid_saopaulo = covid_saopaulo
+            , deaths_saopaulo = C[["deaths_saopaulo"]]
+            , death_change_saopaulo = deaths_change_saopaulo))
 
 # Calcular la variable de respuesta en SAN (¿Aumentó con respecto a ayer?)
 N_t0 = c(NA, SAN[["cases_santiago"]]) # Region Metropolitana (Santiago de Chile)
